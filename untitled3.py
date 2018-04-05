6d608badbf44d8b88fd1c09c29bf09b3978af602
Jsloat0331 <38116570+Jsloat0331@users.noreply.github.com>
2018-04-05 15:29:55 -0600

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 15:28:08 2018
+
+@author: joshua
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 15:06:11 2018
+
+@author: joshua
+"""
+
+
+def read_data():
+    open(battingData1950Present.csv, "r")
+    
+
+
+
+def main():
+   some_value=read_data()
+
+
+
+
+
+
+
+if __name__=='__main__':
+    main()
