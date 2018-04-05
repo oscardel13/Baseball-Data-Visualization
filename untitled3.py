8d341878d65fcec9423302127847a2422dc53375
oscardel13 <oscardel413@gmail.com>
2018-04-05 15:45:11 -0600

    added to read_data funciton

--- untitled3.py
+++ untitled3.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  5 15:28:08 2018
-
-@author: joshua
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  5 15:06:11 2018
-
-@author: joshua
-"""
-
-
+import numpy as np
+from matplotlib import pyplot as plt
+import csv
 def read_data():
-    open(battingData1950Present.csv, "r")
-    
+    textfile = input("Choose your file: ") # gets text file name
+    myfile = open(textfile) # Opens the file
+    myfile_csv=csv.reader(myfile) # makes file more readible
+    players = [] # List for the students
+    for row in myfile_csv:
+        #print(row)
+        players.append(row) # Adds the list of the student into another list
+    players.sort(key=lambda x: (x[0],x[1])) # Alphabetize the list by last name then firstname
+    myfile.close() # Closes the file
+    return players
+
 
 
 
 def main():
    some_value=read_data()
+   print(some_value)
 
 
 
