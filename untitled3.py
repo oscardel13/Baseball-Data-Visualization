094a38ebf588ab5b9bbbcc2fc7e97882214746a8
oscardel13 <oscardel413@gmail.com>
2018-04-13 18:55:16 -0600

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -29,12 +29,11 @@ def get_menu_choice(players):
         print("10: Extra Credit")
         choice = input('> ')
         if ( choice == "1"):
-            hist(unique(players))
+            uniqueHist(unique(players))
         if ( choice == "2"):
-        if ( choice == "2"):
-            postitionHist(UniquePostions(players))
+            postitionHist(UniquePostions(players,1))
         if ( choice == "3"):
-            cutoffHist(UniquePostions(players),100,3000)
+            postitionHist(UniquePostions(players,2))
         if ( choice == "4"):
             scat(teamPresence(players))
         if ( choice == "5"):
@@ -65,14 +64,13 @@ def unique(players):
     Nsums = np.array(sums)
     return Nsums
 
-def hist( Nlist ):
+def uniqueHist( Nlist ):
     plt.xlabel('Number of total runs')
     plt.ylabel('Number of players')
     plt.title('Total runs vs number of players having that many runs')
     plt.hist(Nlist.astype(int), bins = 100, stacked = True)
-    plt.savefig("Hist.pdf")
     plt.show()
-    
+
 def PositionList(list):
     playerID = []
     runs = []
@@ -88,7 +86,23 @@ def PositionList(list):
     Nsums = np.array(sums)
     return Nsums
 
-def UniquePostions(players):
+def PositionListCut(list): #need to edit so you can choose cutoff
+    playerID = []
+    runs = []
+    for row in list:
+        playerID.append(row[0])
+        runs.append(row[1])
+    NplayerID = np.array(playerID)
+    Nruns = np.array(runs)
+    unique_players = np.unique(NplayerID)
+    sums = []
+    for group in unique_players:
+        if (Nruns[NplayerID == group].astype(int).sum() >=100):
+            sums.append(Nruns[NplayerID == group].astype(int).sum())
+    Nsums = np.array(sums)
+    return Nsums
+
+def UniquePostions(players, option):
     sums1B = []
     sums2B = []
     sums3B = []
@@ -114,14 +128,17 @@ def UniquePostions(players):
             sumsP.append([row[1],row[7]])
         if (row[19] == "SS"):
             sumsSS.append([row[1],row[7]])
-    return np.array([PositionList(np.array(sums1B)),PositionList(np.array(sums2B)),PositionList(np.array(sums3B)),PositionList(np.array(sumsC)),PositionList(np.array(sumsNULL)),PositionList(np.array(sumsOF)),PositionList(np.array(sumsP)),PositionList(np.array(sumsSS))])
+    if option == 1:
+        return np.array([PositionList(np.array(sums1B)),PositionList(np.array(sums2B)),PositionList(np.array(sums3B)),PositionList(np.array(sumsC)),PositionList(np.array(sumsNULL)),PositionList(np.array(sumsOF)),PositionList(np.array(sumsP)),PositionList(np.array(sumsSS))])
+    if option == 2:
+        return np.array([PositionListCut(np.array(sums1B)),PositionListCut(np.array(sums2B)),PositionListCut(np.array(sums3B)),PositionListCut(np.array(sumsC)),PositionListCut(np.array(sumsNULL)),PositionListCut(np.array(sumsOF)),PositionListCut(np.array(sumsP)),PositionListCut(np.array(sumsSS))])
+
 def postitionHist(Nlist):
     plt.xlabel('Number of total runs')
     plt.ylabel('Number of players')
     plt.title('Total runs vs number of players having that many runs')
     plt.hist(Nlist, bins = 100, stacked = True, label=["1B","2B","3B","C","NULL","OF","P","SS"])
     plt.legend()
-    plt.savefig("positionHist.pdf")
     plt.show()
     
 def cutoffHist(Nlist,cut1,cut2):
@@ -130,7 +147,6 @@ def cutoffHist(Nlist,cut1,cut2):
     plt.title('Total runs vs number of players having that many runs')
     plt.hist(Nlist, bins = 100, stacked = True, range=(cut1,cut2),label=["1B","2B","3B","C","NULL","OF","P","SS"])
     plt.legend()
-    plt.savefig("cutoffHist.pdf")
     plt.show()
 
 def teamPresence(Nlist):
@@ -139,18 +155,39 @@ def teamPresence(Nlist):
     year = []
     for row in Nlist:
         team.append(row[4])
-        year.append(row[0])
+        year.append(row[0])  
     teamList = np.array([np.array(team),np.array(year)])
     return np.unique(teamList,axis=0)
             
 def scat(Nlist):
-    plt.scatter(Nlist[0],Nlist[1])
+    years = np.unique(Nlist[0])
+
+    plt.figure(figsize=(12,12))
+    
+    np1 = np.asarray(Nlist[1])
+    np0 = np.asarray(Nlist[0], dtype = np.int32)
+    
+    teams = np.unique(np1)
+    height = 0
+    
+    for team in teams:
+        indicies = np.where(np1[:] == team)
+        years = np.unique(np0[indicies])
+        height_array = []
+        for year in years:
+            height_array.append(height)
+        height += 1
+        plt.scatter(years, height_array, marker = "s")
+    
+    plt.yticks(np.arange(len(teams)), teams)   
+
     plt.show()
     
+
+
 def main():
    playersData=np.array(read_data())
    get_menu_choice(playersData)
-
-
+   
 if __name__=='__main__':
     main()
